62d555902ced777b20bc833da6be6359a1c1db1d
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-05 09:59:15 -0300

    Class User e UserResource

---
+++ src/main/java/com/garotinho/course/entities/User.java
@@ -0,0 +1,122 @@
+package com.garotinho.course.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable{ // Serializable : se transforma em cadeia de bytes para trafegar em diversos meios;
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String email;
+    private String fone;
+    private String password;
+
+    public User(){}
+    public User(Long id, String name, String email, String fone, String password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.fone = fone;
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the fone
+     */
+    public String getFone() {
+        return fone;
+    }
+
+    /**
+     * @param fone the fone to set
+     */
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "User [email=" + email + ", fone=" + fone + ", id=" + id + ", name=" + name + ", password=" + password
+                + "]";
+    }
+    
+}
