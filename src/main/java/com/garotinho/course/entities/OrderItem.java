a1e52cc21bb365b2682a2f1b92e6a6478063dd83
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-09 16:33:08 -0300

    Criação do Item de Pedido e Associação Muitos para muitos com chave composta

---
+++ src/main/java/com/garotinho/course/entities/OrderItem.java
@@ -0,0 +1,94 @@
+package com.garotinho.course.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.garotinho.course.entities.pk.OrderItemPK;
+
+@Entity @Table(name="tb_order_item")
+public class OrderItem implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    private OrderItemPK id;
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(){}
+    public OrderItem(Order order, Product product, Integer quantity, Double price){
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    /**
+     * @return Integer return the quantity
+     */
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return Double return the price
+     */
+    public Double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderItem other = (OrderItem) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+    
+}
