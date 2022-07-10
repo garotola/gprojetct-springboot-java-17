d4077e260bcb5081b3cbea6c213c95696ffc79f5
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-10 14:26:12 -0300

    Inserção de User - Metodo POST

--- src/main/java/com/garotinho/course/resources/UserResource.java
+++ src/main/java/com/garotinho/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.garotinho.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.garotinho.course.entities.User;
 import com.garotinho.course.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(value="/users") // caminho
-public class UserResource {
-
+public class UserResource {    
     @Autowired
     private UserService service;
 
-    @GetMapping // users/
+    @GetMapping // users/ // Endpoint
     public ResponseEntity<List<User>> findAll(){
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
@@ -27,6 +32,13 @@ public class UserResource {
     @GetMapping(value="/{id}") // Possivel rota ao entrar no users Ex : users/3
     public ResponseEntity<User> findById(@PathVariable Long id){
         return ResponseEntity.ok().body(service.findById(id));
+    }
+    
+    @PostMapping // Post - enviar dados para o servidor.
+    public ResponseEntity<User> insert(@RequestBody User entity) {
+        entity = service.insert(entity);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(entity.getId()).toUri();
+        return ResponseEntity.created(location).body(entity);
     }
     
 
