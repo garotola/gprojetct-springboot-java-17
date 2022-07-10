fc510726a05dfa4d300cbbe4fad4615f4e6ee56c
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-10 14:33:52 -0300

    Deleção de User - Método DELETE

--- src/main/java/com/garotinho/course/resources/UserResource.java
+++ src/main/java/com/garotinho/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,12 @@ public class UserResource {
         return ResponseEntity.created(location).body(entity);
     }
     
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
+    //PathVariable - Variavel da Url
+    //RequestBody - variavel do corpo da requisicao
 }
