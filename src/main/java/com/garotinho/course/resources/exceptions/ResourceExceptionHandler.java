38a887067add7bc51fc988bfe30063326b3de04c
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-11 13:08:27 -0300

    Exceção do método de deleção do user

--- src/main/java/com/garotinho/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/garotinho/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.garotinho.course.services.exceptions.DataBaseException;
 import com.garotinho.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice // Reconhece as exceções que acontecem e faz um tratamento.
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         String error  = "Recurso não encontrado.";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError bodyError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(bodyError);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+        String error  = "Error no Banco de dados.";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError bodyError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(bodyError);
     }
--- src/main/java/com/garotinho/course/services/UserService.java
+++ src/main/java/com/garotinho/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.garotinho.course.entities.User;
 import com.garotinho.course.repositories.UserRepository;
+import com.garotinho.course.services.exceptions.DataBaseException;
 import com.garotinho.course.services.exceptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }catch(EmptyResultDataAccessException error){
+            throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException error){
+            throw new DataBaseException("Não mate os pais sem matar os filhos");
+        }
     }
 
     public User update(Long id, User user){
