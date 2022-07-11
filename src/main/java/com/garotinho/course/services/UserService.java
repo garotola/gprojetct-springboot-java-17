7aad3eb9ba94caed2e05c6632bb0850027f716c6
Rafael Farias <rafaelfariasprogramador@gmail.com>
2022-07-11 13:12:08 -0300

    Exceção de Atualização do user

--- src/main/java/com/garotinho/course/services/UserService.java
+++ src/main/java/com/garotinho/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.garotinho.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +46,13 @@ public class UserService {
     }
 
     public User update(Long id, User user){
-        User entity = repository.getOne(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try{
+            User entity = repository.getOne(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        }catch(EntityNotFoundException error){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
